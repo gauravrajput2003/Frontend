20324f13dd8b135cae5decab2ff12102baeca9e4
Your Name <gauravkumarsingh010103@gmail.com>
2025-08-14 21:19:40 +0530

    donee

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,5 +1,5 @@
 // For local development
-//export const BASE_URL = "http://localhost:9931";
+//export const BASE_URL = "http://localhost:9931/api";
 
 // For production (deployed)
 export const BASE_URL = "/api";
