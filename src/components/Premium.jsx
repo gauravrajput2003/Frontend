b9c890b1a3aa90de1aca93d8f4fec2300e1c47f0
Your Name <gauravkumarsingh010103@gmail.com>
2025-08-18 01:34:27 +0530

    razorpay

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,14 +1,36 @@
 import React from 'react'
+import axios from 'axios'
 import { BASE_URL } from '../utils/Constant'
 
 const Premium = () => {
   const handleBuyClick=async(type)=>{
-const order=await axios.post(
-  BASE_URL+"/payment/create",
-  {type,},
-  {withCredentials:true}
+const order = await axios.post(
+  BASE_URL + "/payment/create",
+  { membershipType: type },
+  { withCredentials: true }
 );
+const { keyId, order: { id: orderId, amount, currency, notes, receipt } } = order.data;
+const options = {
+    "key": keyId,
+    "amount": amount,
+    "currency": currency,
+    "name": "devNexus", 
+    "description": "connect to other",
 
+    "order_id": orderId,
+
+    "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information especially their phone number
+        "name": notes.firstName + " " + notes.lastName, //your customer's name
+        "email": notes.email,
+  
+    },
+ 
+    "theme": {
+        "color": "#3399cc"
+    }
+};
+const rzp = new window.Razorpay(options);
+rzp.open();
   }
   return (
     <div className='mt-40 p-10'>
