a21604e50cba57533f6e06472c54b95d9ce3c025
Your Name <gauravkumarsingh010103@gmail.com>
2025-08-17 21:28:30 +0530

    fix error

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: '/var/www/html'  // Build directly to web directory
-  }
+  build: { outDir: 'dist' }
 })
